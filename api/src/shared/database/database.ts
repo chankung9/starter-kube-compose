8030479dc9339a4a9f0ad5168254bcc9297b43f1
worrapong limsupreeyalat <chankung9@gmail.com>
2021-03-29 18:51:06 +0700

    dbhost

--- api/src/shared/database/database.ts
+++ api/src/shared/database/database.ts
@@ -8,7 +8,7 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
-      host: 'db',
+      host: process.env.DB_HOST,
       port: +process.env.DB_PORT, // parse to number
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
